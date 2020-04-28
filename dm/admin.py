2a4dcef13b5d25bdb3c05e8cdc6a0f2ddb375279
CFE <hello@teamcfe.com>
2020-04-27 21:30:45 -0700

    6 - The Channel Model

--- dm/admin.py
+++ dm/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import ChannelMessage
 
-admin.site.register(Message)
+admin.site.register(ChannelMessage)
--- dm/models.py
+++ dm/models.py
@@ -17,8 +17,20 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Message(BaseModel):
+class ChannelMessage(BaseModel):
+    channel = models.ForeignKey("Channel", null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
 
-# class Channel(BaseModel): # models.model
+
+class ChannelUser(BaseModel):
+    channel = models.ForeignKey("Channel", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # permissions? 
+
+class Channel(BaseModel): # models.model
+    # slack-like
+    # 1 user
+    # 2 users
+    # 2+ users
+    users = models.ManyToManyField(User, blank=True, through=ChannelUser)
