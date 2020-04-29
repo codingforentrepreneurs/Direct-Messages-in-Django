7d0be5f231595f30ebe037c68f22d5a2842eee6c
CFE <hello@teamcfe.com>
2020-04-29 16:05:02 -0700

    17 - URL Regex for UUID

--- cfe_dm/urls.py
+++ cfe_dm/urls.py
@@ -21,8 +21,11 @@ from dm.views import (
     PrivateMessageDetailView
 )
 
+UUID_CHANNEL_REGEX = r'channel/(?P<pk>[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12})'
+
 urlpatterns = [
-    re_path(r'channel/(?P<pk>[\w-]+)', ChannelDetailView.as_view()),
+    re_path(UUID_CHANNEL_REGEX, ChannelDetailView.as_view()),
+    # re_path(r'channel/(?P<slug>[\w-]+)', ChannelSlugLookupView.as_view()),
     path("dm/<str:username>", PrivateMessageDetailView.as_view()),
     path('admin/', admin.site.urls),
 ]
